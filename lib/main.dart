f8a360988cdbc9741549c9fef556edfa8498476b
lanxin2578 <765752270@qq.com>
2019-10-29 00:35:36 +0800

    update
    update

--- lib/main.dart
+++ lib/main.dart
@@ -47,28 +47,53 @@ class _MyHomePageState extends State<MyHomePage> {
 
   TimerUtil usbSerialTimer;
 
-  bool f1 = false;
-  bool f2 = false;
-  bool f3 = false;
-  bool f4 = false;
-  bool f5 = false;
-  bool f6 = false;
-  bool f7 = false;
-  bool f8 = false;
-  bool f9 = false;
-  bool f10 = false;
-  bool f11 = false;
-  bool f12 = false;
-  bool f13 = false;
-  bool f14 = false;
-  bool f15 = false;
-  bool f16 = false;
-  bool f17 = false;
-  bool f18 = false;
-  bool f19 = false;
-  bool f20 = false;
-  bool f21 = false;
-  bool f22 = false;
+  List<bool> bools = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+
+  // bool f1 = false;
+  // bool f2 = false;
+  // bool f3 = false;
+  // bool f4 = false;
+  // bool f5 = false;
+  // bool f6 = false;
+  // bool f7 = false;
+  // bool f8 = false;
+  // bool f9 = false;
+  // bool f10 = false;
+  // bool f11 = false;
+  // bool f12 = false;
+  // bool f13 = false;
+  // bool f14 = false;
+  // bool f15 = false;
+  // bool f16 = false;
+  // bool f17 = false;
+  // bool f18 = false;
+  // bool f19 = false;
+  // bool f20 = false;
+  // bool f21 = false;
+  // bool f22 = false;
 
   int femaleTotal = 14;
   int femaleUsing = 0;
@@ -82,12 +107,25 @@ class _MyHomePageState extends State<MyHomePage> {
   int todayFemale = 0;
   int todayMan = 0;
 
+  int smileNum = 0;
+  int normalNum = 0;
+  int sadNum = 0;
+
   // 1-14 是女厕
   // 15-20 是男厕
   // 21-22 残疾人位
   @override
   void initState() {
     super.initState();
+
+    //初始化今日访问人数
+    todayFemale = SpUtil.getInt("todayFemale") ?? 0;
+    todayMan = SpUtil.getInt("todayMan") ?? 0;
+
+    smileNum = SpUtil.getInt("smileNum") ?? 0;
+    normalNum = SpUtil.getInt("normalNum") ?? 0;
+    sadNum = SpUtil.getInt("sadNum") ?? 0;
+
     timer = TimerUtil();
     timer.setInterval(1000);
     timer.setOnTimerTickCallback((i) {
@@ -96,6 +134,13 @@ class _MyHomePageState extends State<MyHomePage> {
             DateUtil.formatDate(DateTime.now(), format: DataFormats.zh_y_mo_d);
         this.time =
             DateUtil.formatDate(DateTime.now(), format: DataFormats.h_m_s);
+        //零点清空男女厕今日汇总数据
+        if(this.time == "00:00:00") {
+          this.todayFemale = 0;
+          this.todayMan = 0;
+          SpUtil.putInt("todayFemale", 0);
+          SpUtil.putInt("todayMan", 0);
+        }
       });
     });
     if (timer != null) {
@@ -108,9 +153,8 @@ class _MyHomePageState extends State<MyHomePage> {
     usbSerialTimer.setOnTimerTickCallback((i) {});
     if (usbSerialTimer != null) {}
   }
-
-  //请求坑位信息
-  void fetchData1() async {
+  
+  void fetchData() async {
     List<UsbDevice> devices = await UsbSerial.listDevices();
     print(devices);
 
@@ -125,11 +169,16 @@ class _MyHomePageState extends State<MyHomePage> {
       print("Failed to open");
       return;
     }
+    fetchData1(port);
+  }
 
+  //请求坑位信息
+  void fetchData1(UsbPort port) async {
     await port.setDTR(true);
     await port.setRTS(true);
 
-    port.setPortParameters(115200, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+    port.setPortParameters(
+        115200, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
 
     // print first result and close port.
     // 01 01 01 00 xx xx
@@ -137,14 +186,52 @@ class _MyHomePageState extends State<MyHomePage> {
     port.inputStream.listen((Uint8List event) {
       print(event);
       int useFlag = event[3];
-      if (useFlag == 0x00) {
-        //没有人
+      int address = event[0];
+      setState(() {
+        //女厕位
+        if (address <= 14) {  //女厕位
+          if(useFlag==0x01 && !this.bools[address-1]) {
+            this.femaleUsing = this.femaleUsing + 1;
+            this.todayFemale += this.todayFemale;
+            //保存今日女厕总人数
+            SpUtil.putInt("todayFemale", todayFemale);
+          }else if(useFlag!=0x01 && this.bools[address-1]) {
+            this.femaleUsing = this.femaleUsing - 1;
+          }
+        }else if(address <= 20) { //男厕位
+          if(useFlag==0x01 && !this.bools[address-1]) {
+            this.manUsing = this.manUsing + 1;
+            this.todayMan += this.todayMan; 
+            //保存今日男厕总人数
+            SpUtil.putInt("todayMan", todayMan);
+          }else if(useFlag!=0x01 && this.bools[address-1]) {
+            this.manUsing = this.manUsing - 1;
+          }
+        }else if(address <= 22) { //残疾厕位
+          if(useFlag==0x01 && !this.bools[address-1]) {
+            this.canjiUsing = this.canjiUsing + 1;
+          }else if(useFlag!=0x01 && this.bools[address-1]) {
+            this.canjiUsing = this.canjiUsing - 1;
+          }
+        }
+        this.bools[address-1] = useFlag==0x01 ? true : false;
+        this.bools = List.from(bools);
+      });
 
-      } else if (useFlag == 0x01) {
-        //有人
-
-      }
-      port.close();
+      //switch (address) {
+        // case 0x01: {
+        //   setState(() {
+        //     if(useFlag==0x01 && !this.f1) {
+        //       this.femaleUsing = this.femaleUsing + 1;
+        //     }else if(useFlag!=0x01 && this.f1) {
+        //       this.femaleUsing = this.femaleUsing - 1;
+        //     }
+        //     this.f1 = useFlag==0x01 ? true : false;
+        //   });
+        // }
+        // break;
+      //}
+      
     });
 
     //请求每个坑位的使用情况
@@ -176,7 +263,33 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   //请求评价信息
-  void fetchData2() {}
+  //5A A5 06 83 00 00 02 00 01 满意++
+  //5A A5 06 83 00 00 02 00 02 一般++
+  //5A A5 06 83 00 00 02 00 03 不满意++
+  void fetchData2(UsbPort port) async {
+    await port.setDTR(true);
+    await port.setRTS(true);
+    port.setPortParameters(
+        115200, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+    // print first result and close port.
+    // 01 01 01 00 xx xx
+    // 地址 功能码 数据长度 数据 CRC校验
+    port.inputStream.listen((Uint8List event) {
+      print(event);
+      int satisfaction = event[8];
+      if(satisfaction==0x01) {
+        smileNum += smileNum;
+        SpUtil.putInt("smileNum", smileNum);
+      }else if(satisfaction==0x02) {
+        normalNum += normalNum;
+        SpUtil.putInt("normalNum", normalNum);
+      }else if(satisfaction==0x03) {
+        sadNum += sadNum;
+        SpUtil.putInt("sadNum", sadNum);
+      }
+    });
+  }
 
   @override
   void dispose() {
@@ -215,7 +328,7 @@ class _MyHomePageState extends State<MyHomePage> {
         Positioned(
           top: 128,
           left: 225,
-          child: Num(number: (femaleTotal-femaleUsing).toString()),
+          child: Num(number: (femaleTotal - femaleUsing).toString()),
         ),
 
 /////////////男厕总侧位/////////////
@@ -234,7 +347,7 @@ class _MyHomePageState extends State<MyHomePage> {
         Positioned(
           top: 207,
           left: 225,
-          child: Num(number: (manTotal-manUsing).toString()),
+          child: Num(number: (manTotal - manUsing).toString()),
         ),
 
 /////////////残疾人总侧位/////////////
@@ -253,7 +366,7 @@ class _MyHomePageState extends State<MyHomePage> {
         Positioned(
           top: 280,
           left: 225,
-          child: Num(number: (canjiTotal-canjiUsing).toString()),
+          child: Num(number: (canjiTotal - canjiUsing).toString()),
         ),
 
 /////////////左下角///////////////////
@@ -295,126 +408,222 @@ class _MyHomePageState extends State<MyHomePage> {
         Positioned(
           left: 339,
           top: 168,
-          child: f1 ? SizeImage(url: red,) : SizeImage(),
+          child: bools[0]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(),
         ),
         //f2
         Positioned(
           left: 375,
           top: 168,
-          child: f2 ? SizeImage(url: red,) : SizeImage(),
+          child: bools[1]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(),
         ),
         //f3
         Positioned(
           left: 411,
           top: 168,
-          child: f3 ? SizeImage(url: red,) : SizeImage(),
+          child: bools[2]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(),
         ),
         //f4
         Positioned(
           left: 450,
           top: 168,
-          child: f4 ? SizeImage(url: red,) : SizeImage(),
+          child: bools[3]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(),
         ),
         //f5
         Positioned(
           left: 493,
           top: 168,
-          child: f5 ? SizeImage(url: red,) : SizeImage(),
+          child: bools[4]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(),
         ),
         //f6
         Positioned(
           left: 529,
           top: 168,
-          child: f6 ? SizeImage(url: red,) : SizeImage(),
+          child: bools[5]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(),
         ),
-
-
 
         Positioned(
           left: 570,
           top: 168,
-          child: f15 ? SizeImage(url: red,) : SizeImage(),
+          child: bools[14]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(),
         ),
         Positioned(
           left: 606,
           top: 168,
-          child: f16 ? SizeImage(url: red,) : SizeImage(),
+          child: bools[15]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(),
         ),
         Positioned(
           left: 641,
           top: 168,
-          child: f17 ? SizeImage(url: red,) : SizeImage(),
+          child: bools[16]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(),
         ),
 
         ///////////// 第二排 /////////////
         Positioned(
           left: 388,
           top: 270,
-          child: f7 ? SizeImage(url: red_r,) : SizeImage(url: green_r,),
+          child: bools[6]
+              ? SizeImage(
+                  url: red_r,
+                )
+              : SizeImage(
+                  url: green_r,
+                ),
         ),
         Positioned(
           left: 424,
           top: 270,
-          child: f8 ? SizeImage(url: red_r,) : SizeImage(url: green_r,),
+          child: bools[7]
+              ? SizeImage(
+                  url: red_r,
+                )
+              : SizeImage(
+                  url: green_r,
+                ),
         ),
         Positioned(
           left: 459,
           top: 270,
-          child: f9 ? SizeImage(url: red_r,) : SizeImage(url: green_r,),
+          child: bools[8]
+              ? SizeImage(
+                  url: red_r,
+                )
+              : SizeImage(
+                  url: green_r,
+                ),
         ),
         Positioned(
           left: 493,
           top: 270,
-          child: f10 ? SizeImage(url: red_r,) : SizeImage(url: green_r,),
+          child: bools[9]
+              ? SizeImage(
+                  url: red_r,
+                )
+              : SizeImage(
+                  url: green_r,
+                ),
         ),
         Positioned(
           left: 528,
           top: 270,
-          child: f11 ? SizeImage(url: red_r,) : SizeImage(url: green_r,),
+          child: bools[10]
+              ? SizeImage(
+                  url: red_r,
+                )
+              : SizeImage(
+                  url: green_r,
+                ),
         ),
-
-
 
         Positioned(
           left: 570,
           top: 270,
-          child: f18 ? SizeImage(url: red_r,) : SizeImage(url: green_r,),
+          child: bools[17]
+              ? SizeImage(
+                  url: red_r,
+                )
+              : SizeImage(
+                  url: green_r,
+                ),
         ),
         Positioned(
           left: 605,
           top: 270,
-          child: f19 ? SizeImage(url: red_r,) : SizeImage(url: green_r,),
+          child: bools[18]
+              ? SizeImage(
+                  url: red_r,
+                )
+              : SizeImage(
+                  url: green_r,
+                ),
         ),
         Positioned(
           left: 641,
           top: 270,
-          child: f20 ? SizeImage(url: red_r,) : SizeImage(url: green_r,),
+          child: bools[19]
+              ? SizeImage(
+                  url: red_r,
+                )
+              : SizeImage(
+                  url: green_r,
+                ),
         ),
 
         /////////// 第三排 ///////////
         Positioned(
           left: 388,
           top: 328,
-          child: f12 ? SizeImage(url: red,) : SizeImage(url: green,),
+          child: bools[11]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(
+                  url: green,
+                ),
         ),
         Positioned(
           left: 424,
           top: 328,
-          child: f13 ? SizeImage(url: red,) : SizeImage(url: green,),
+          child: bools[12]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(
+                  url: green,
+                ),
         ),
         Positioned(
           left: 458,
           top: 328,
-          child: f14 ? SizeImage(url: red,) : SizeImage(url: green,),
+          child: bools[13]
+              ? SizeImage(
+                  url: red,
+                )
+              : SizeImage(
+                  url: green,
+                ),
         ),
-
-
 
         Positioned(
           left: 500,
           top: 298,
           child: SizeImage(
-            url: f21 ? "assets/images/4_01.png" : "assets/images/3_01.png",
+            url: bools[20] ? "assets/images/4_01.png" : "assets/images/3_01.png",
             width: 30,
             height: 68,
           ),
@@ -423,7 +632,7 @@ class _MyHomePageState extends State<MyHomePage> {
           left: 528,
           top: 304,
           child: SizeImage(
-            url: f22 ? "assets/images/4_02.png" : "assets/images/3_02.png",
+            url: bools[21] ? "assets/images/4_02.png" : "assets/images/3_02.png",
             width: 30,
             height: 58,
           ),
@@ -434,7 +643,7 @@ class _MyHomePageState extends State<MyHomePage> {
           left: 788,
           top: 350,
           child: Num(
-            number: "45.1%",
+            number: (smileNum+normalNum+sadNum)==0 ? "0.0%" : (smileNum/(smileNum+normalNum+sadNum)).toStringAsFixed(1)+"%",
             size: 12,
           ),
         ),
@@ -442,7 +651,7 @@ class _MyHomePageState extends State<MyHomePage> {
           left: 843,
           top: 350,
           child: Num(
-            number: "45.1%",
+            number: (smileNum+normalNum+sadNum)==0 ? "0.0%" : (normalNum/(smileNum+normalNum+sadNum)).toStringAsFixed(1)+"%",
             size: 12,
           ),
         ),
@@ -450,7 +659,7 @@ class _MyHomePageState extends State<MyHomePage> {
           left: 898,
           top: 350,
           child: Num(
-            number: "45.1%",
+            number: (smileNum+normalNum+sadNum)==0 ? "0.0%" : (sadNum/(smileNum+normalNum+sadNum)).toStringAsFixed(1)+"%",
             size: 12,
           ),
         ),
