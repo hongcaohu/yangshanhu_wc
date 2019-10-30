33ab60848212832ab8b21e7a5256f0f674a1ac21
lanxin2578 <765752270@qq.com>
2019-10-30 15:41:37 +0800

    update
    update

--- lib/utils/dioUtil.dart
+++ lib/utils/dioUtil.dart
@@ -3,9 +3,14 @@ import 'package:dio/dio.dart';
 class DioUtils {
 
   String logUrl = "http://106.14.170.234:8081/uploadLogs";
+  String upgradeUrl = "http://106.14.170.234:8081/upgrade";
   Dio dio = new Dio();
 
   void post(String body) async {
     await dio.post(logUrl, data: {"content": body});
+  }
+
+  reqUpgradeInfo() async {
+    return await dio.get(upgradeUrl);
   }
 }
